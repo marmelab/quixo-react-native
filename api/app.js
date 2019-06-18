745d66aa84765071d66a760faa3d6dc02d79b1a4
Pierre Haller <pierre@marmelab.com>
2019-06-18 17:56:23 +0200

    Remove rest route

--- api/app.js
+++ api/app.js
@@ -1,17 +1,15 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const { PORT } = require('./constants/api');
-const createNewGame = require('./app/createNewGame');
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const { PORT } = require("./constants/api");
+const createNewGame = require("./app/createNewGame");
 
-const listenerSocketIO = (socket) => {
-  socket.on('new-game', () => createNewGame().then(game => socket.emit('new-game-reply', game)));
+const listenerSocketIO = socket => {
+  socket.on("new-game", () =>
+    createNewGame().then(game => socket.emit("new-game-reply", game))
+  );
 };
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 http.listen(PORT, () => {});
 
-io.on('connection', socket => listenerSocketIO(socket));
+io.on("connection", socket => listenerSocketIO(socket));
