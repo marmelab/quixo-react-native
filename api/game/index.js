0aef7770ef8e2cabb7807b92c45186e6f3269403
Pierre Haller <pierre@marmelab.com>
2019-06-20 10:48:59 +0200

    Add game logic to move a cube and tests

--- api/game/index.js
+++ api/game/index.js
@@ -1,14 +1,131 @@
 const CROSS_PLAYER = 1;
+const NEUTRAL_CUBE = 0;
 
-const getEmptyBoard = (rows, cols) => Array(rows)
-  .fill()
-  .map(() => Array(cols).fill(0));
+const getEmptyBoard = (rows, cols) =>
+  Array(rows)
+    .fill()
+    .map(() => Array(cols).fill(0));
 
-const newGame = (rows = 5, cols = 5) => ({
-  board: getEmptyBoard(rows, cols),
+const newGame = (rows = 5, cols = 5, board = getEmptyBoard(rows, cols)) => ({
+  board,
   rows,
   cols,
-  currentPlayer: CROSS_PLAYER,
+  currentPlayer: CROSS_PLAYER
 });
 
-module.exports = { getEmptyBoard, newGame };
+const getArrayOfCubes = ({ board }) =>
+  board
+    .map((row, x) => row.map((value, y) => ({ x, y, value })))
+    .reduce((mergedRow, row) => [...mergedRow, ...row], []);
+
+const isOutsideCube = ({ x, y, size }) =>
+  x === 0 || y === 0 || x === size - 1 || y === size - 1;
+
+const cubeBelongsToPlayer = ({ cubeValue, playerTeam }) =>
+  cubeValue === NEUTRAL_CUBE || cubeValue === playerTeam;
+
+const getMovablesCubes = ({ board, rows: size, currentPlayer }) =>
+  getArrayOfCubes({ board }).filter(
+    cube =>
+      isOutsideCube({ ...cube, size }) &&
+      cubeBelongsToPlayer({
+        cubeValue: cube.value,
+        playerTeam: currentPlayer
+      })
+  );
+
+const getAvailablesDestinations = ({ board, selectedCube, rows: size }) => {
+  const destinations = [];
+  const { x, y } = selectedCube;
+  const isOnStart = index => index === 0;
+  const isOnEnd = index => index === size - 1;
+  const isOnEdge = index => isOnStart(index) || isOnEnd(index);
+  const indexStart = 0;
+  const indexEnd = size - 1;
+  // If on the top or the bottom of the board, else it's on the left or right
+  if (isOnEdge(x)) {
+    // If not on the left of the board, we can move it to the left
+    if (!isOnStart(y)) {
+      destinations.push({ x, y: indexStart });
+    }
+    // If not on the right of the board, we can move it to the right
+    if (!isOnEnd(y)) {
+      destinations.push({ x, y: indexEnd });
+    }
+    // If on the top of the board, we can move it to the bottom, else to the top
+    if (isOnStart(x)) {
+      destinations.push({ x: indexEnd, y });
+    } else {
+      destinations.push({ x: indexStart, y });
+    }
+  } else if (isOnEdge(y)) {
+    if (!isOnStart(x)) {
+      destinations.push({ x: indexStart, y });
+    }
+    if (!isOnEnd(x)) {
+      destinations.push({ x: indexEnd, y });
+    }
+    if (isOnStart(y)) {
+      destinations.push({ x, y: indexEnd });
+    } else {
+      destinations.push({ x, y: indexStart });
+    }
+  }
+  return destinations;
+};
+
+const moveCube = ({ board, coordsStart, coordsEnd, player }) => {
+  const newBoard = copyBoard(board);
+  if (coordsStart.x === coordsEnd.x) {
+    return shiftLine({
+      board: newBoard,
+      yStart: coordsStart.y,
+      yEnd: coordsEnd.y,
+      rowIndex: coordsStart.x,
+      player
+    });
+  }
+  if (coordsStart.y === coordsEnd.y) {
+    const flippedBoard = flipRowsAndCols(newBoard);
+    return flipRowsAndCols(
+      shiftLine({
+        board: flippedBoard,
+        yStart: coordsStart.x,
+        yEnd: coordsEnd.x,
+        rowIndex: coordsStart.y,
+        player
+      })
+    );
+  }
+};
+
+const shiftLine = ({ board, yStart, yEnd, rowIndex, player }) => {
+  const newBoard = copyBoard(board);
+  const [step, indexBound] = yEnd < yStart ? [1, yStart + 1] : [-1, yStart - 1];
+  let value = player;
+  for (let index = yEnd; index != indexBound; index += step) {
+    const tmpValue = newBoard[rowIndex][index];
+    newBoard[rowIndex][index] = value;
+    value = tmpValue;
+  }
+  return newBoard;
+};
+
+const flipRowsAndCols = board =>
+  board[0].map((col, i) => board.map(row => row[i]));
+
+const copyBoard = board => board.map(line => line.slice());
+
+module.exports = {
+  getEmptyBoard,
+  newGame,
+  getArrayOfCubes,
+  isOutsideCube,
+  cubeBelongsToPlayer,
+  getMovablesCubes,
+  getAvailablesDestinations,
+  moveCube,
+  shiftLine,
+  flipRowsAndCols,
+  copyBoard
+};
--- api/tests.js/game/game.test.js
+++ api/tests.js/game/game.test.js
@@ -1,7 +1,118 @@
-const { getEmptyBoard } = require("../../game");
+const {
+  getEmptyBoard,
+  newGame,
+  getMovablesCubes,
+  getAvailablesDestinations,
+  moveCube
+} = require("../../game");
 
 test("Get an empty board of size 5", () => {
   const board = getEmptyBoard(5, 5);
   expect(board).toHaveLength(5);
   board.forEach(line => expect(line).toHaveLength(5));
+});
+
+test("Get movables cubes from empty board", () => {
+  const game = newGame();
+  const expectedMovables = [
+    { x: 0, y: 0, value: 0 },
+    { x: 0, y: 1, value: 0 },
+    { x: 0, y: 2, value: 0 },
+    { x: 0, y: 3, value: 0 },
+    { x: 0, y: 4, value: 0 },
+    { x: 1, y: 0, value: 0 },
+    { x: 1, y: 4, value: 0 },
+    { x: 2, y: 0, value: 0 },
+    { x: 2, y: 4, value: 0 },
+    { x: 3, y: 0, value: 0 },
+    { x: 3, y: 4, value: 0 },
+    { x: 4, y: 0, value: 0 },
+    { x: 4, y: 1, value: 0 },
+    { x: 4, y: 2, value: 0 },
+    { x: 4, y: 3, value: 0 },
+    { x: 4, y: 4, value: 0 }
+  ];
+  const movables = getMovablesCubes(game);
+  expect(movables).toEqual(expectedMovables);
+});
+
+test("Get movables cubes from full board with one movable cube", () => {
+  const game = newGame(5, 5, [
+    [1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1]
+  ]);
+
+  const expectedMovables = [{ x: 0, y: 0, value: 1 }];
+
+  const movables = getMovablesCubes(game);
+  expect(movables).toEqual(expectedMovables);
+});
+
+test("Get destinations from corner", () => {
+  const game = newGame();
+  const selectedCube = { x: 0, y: 0 };
+
+  const exepectedDestinations = [{ x: 0, y: 4 }, { x: 4, y: 0 }];
+
+  const destinations = getAvailablesDestinations({ ...game, selectedCube });
+  expect(destinations).toEqual(exepectedDestinations);
+});
+
+test("Move cube from middle with shift on row", () => {
+  const game = newGame(5, 5, [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+  ]);
+  const coordsStart = { x: 2, y: 0 };
+  const coordsEnd = { x: 2, y: 4 };
+
+  const expectedBoard = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 1],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+  ];
+  const board = moveCube({
+    board: game.board,
+    coordsStart,
+    coordsEnd,
+    player: game.currentPlayer
+  });
+
+  expect(board).toEqual(expectedBoard);
+});
+
+test("Move cube from middle with shift on col", () => {
+  const game = newGame(5, 5, [
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0]
+  ]);
+  const coordsStart = { x: 0, y: 2 };
+  const coordsEnd = { x: 4, y: 2 };
+
+  const expectedBoard = [
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0]
+  ];
+  const board = moveCube({
+    board: game.board,
+    coordsStart,
+    coordsEnd,
+    player: game.currentPlayer
+  });
+
+  expect(board).toEqual(expectedBoard);
 });
