6fba2d6af32eaea7d239ee58d04f121b87d09e70
Pierre Haller <pierre@marmelab.com>
2019-06-27 14:45:54 +0200

    Add cors and docker network

--- api/app.js
+++ api/app.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const { PORT } = require("./constants/api");
 const router = require("./router");
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 router(app);
