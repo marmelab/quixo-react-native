3bb708e7efb82c894575223fe3fb21393e2c4ded
Pierre Haller <pierre@marmelab.com>
2019-07-01 15:14:23 +0200

    Change allowed headers

--- api/app.js
+++ api/app.js
@@ -10,6 +10,12 @@ const app = express();
 app.use((req, res) =>
   res.header("Access-Control-Allow-Origin", req.headers.origin)
 );
+app.use((req, res) =>
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, Cache-Control, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  )
+);
 app.use(bodyParser.json());
 
 router(app);
